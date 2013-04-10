5bcfe6fbdf542c541bd5cba2955a6c08e6a36a4a
Joel <joel@otte.rs>
2013-04-10 16:52:00 -0700

    more tests

--- test/integration/match_reporter_test.rb
+++ test/integration/match_reporter_test.rb
@@ -47,7 +47,6 @@ class MatchReporterTest < ActionDispatch::IntegrationTest
 
     teardown do
       find(".delete-goal").click
-      assert page.has_no_selector? ".goals div"
     end
 
     should "be able to create or delete goals" do
@@ -55,7 +54,7 @@ class MatchReporterTest < ActionDispatch::IntegrationTest
       assert page.has_selector? ".goals .home"
     end
 
-    should "be able to input a valid time" do
+    should "be able to input a time" do
       make_a_goal
       within(".goals") do
         find(".goal-time").set("1:00")
@@ -75,12 +74,43 @@ class MatchReporterTest < ActionDispatch::IntegrationTest
       visit(current_path)
       assert_not_equal find(".goals .goal-time").value, "0:60"
     end
+
+    should "be able to input a half" do
+      make_a_goal
+      within(".goals") do
+        find(".goal-half").set("2")
+      end
+      page.execute_script("$('.goals .goal-half').blur()")
+      visit(current_path)
+      assert_equal find(".goals .goal-half").value, "2"
+    end
+    
+    should "not be able to input an invalid half" do
+      make_a_goal
+      within(".goals") do
+        find(".goal-half").set("6")
+      end
+      page.execute_script("$('.goals .goal-half').blur()")
+      assert find("ul.errors li").has_content? "Half isn't a valid half"
+      visit(current_path)
+      assert_not_equal find(".goals .goal-half").value, "6"
+    end
+
+    should "be able to input a time greater than 5 minutes in overtime" do
+      make_a_goal
+      within(".goals") do
+        find(".goal-half").set("3")
+        find(".goal-time").set("10:00")
+      end
+      page.execute_script("$('.goals .goal-half').blur()")
+      assert page.has_no_css?("ul.errors li")
+    end
     
     should "be able to select a scorer" do
       make_a_goal
       page.find ".scorer"
       var = page.evaluate_script("$('.scorer')[0].outerHTML")
-      options = Nokogiri::XML(var).children[0].children.map(&:text).reject{|x|x=="\n"}
+      options = option_values_of "scorer"
       within(".scorer") do
         select options[1]
       end
@@ -92,16 +122,36 @@ class MatchReporterTest < ActionDispatch::IntegrationTest
       make_a_goal
       page.find ".scorer"
       var = page.evaluate_script("$('.scorer')[0].outerHTML")
-      options = Nokogiri::XML(var).children[0].children.map(&:text).reject{|x|x=="\n"}
+      options = option_values_of "scorer"
       within(".scorer") do
         select options[1]
       end
       assert page.has_no_xpath?("//select[@class = 'first-assist']/option[text() = '#{options[1]}']")
+    end
+
+    should "have the first assist automatically reset if the scorer is the same person" do
+      make_a_goal
+      page.find ".scorer"
+      var = page.evaluate_script("$('.scorer')[0].outerHTML")
+      options = Nokogiri::XML(var).children[0].children.map(&:text).reject{|x|x=="\n"}
+      within(".first-assist") do
+        select options[1]
+      end
+      assert_equal find(:xpath, "//select[@class = 'first-assist']/option[@selected = 'selected']").text, options[1]
+      within(".scorer") do
+        select options[1]
+      end
+      assert page.has_no_xpath?("//select[@class = 'first-assist']/option[@selected = 'selected']")
     end
   end
 
   private
   def make_a_goal
     find(".buttons .home-team").click_button "Goal+"
+  end
+
+  def option_values_of klass
+    page.find ".#{klass}"
+    Nokogiri::XML(page.evaluate_script("$('.#{klass}')[0].outerHTML")).children[0].children.map(&:text).reject{|x|x=="\n"}
   end
 end
